a90bdf5966f66097eb11bef02b67278f91a3c375
TAKAHASHI Satoshi <hikobae@gmail.com>
2020-03-30 11:56:39 +0900

    Add zip.Archive()

---
+++ archive/zip/archive.go
@@ -0,0 +1,93 @@
+package zip
+
+import (
+	"archive/zip"
+	ziplib "archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/afero"
+)
+
+// Archive archive files in srcDir to dstFile
+//
+// dstDir の構成が以下のとき, dstFile (zip) に以下のように圧縮する.
+//
+// dstDir/
+// |- x.txt
+// :
+//
+// dstFile (zip)
+// |- x.txt
+// :
+func Archive(fs afero.Fs, srcDir, dstFile string) (err error) {
+	af := afero.Afero{Fs: fs}
+
+	var exists bool
+	exists, err = af.Exists(dstFile)
+	if err != nil {
+		return
+	}
+	if exists {
+		return fmt.Errorf("Destination file already exists: %q", dstFile)
+	}
+
+	var f afero.File
+	f, err = af.Create(dstFile)
+	if err != nil {
+		return
+	}
+	defer er(f.Close, &err)
+
+	w := zip.NewWriter(f)
+	defer er(w.Close, &err)
+
+	af.Walk(srcDir, func(path string, info os.FileInfo, incomingErr error) (err error) {
+		if incomingErr != nil {
+			err = incomingErr
+			return
+		}
+
+		var header *ziplib.FileHeader
+		header, err = zip.FileInfoHeader(info)
+		if err != nil {
+			return
+		}
+
+		var rel string
+		rel, err = filepath.Rel(path, srcDir)
+		if err != nil {
+			return err
+		}
+		header.Name = filepath.ToSlash(rel)
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		var h io.Writer
+		h, err = w.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		var f afero.File
+		f, err = af.Open(path)
+		if err != nil {
+			return
+		}
+		defer er(f.Close, &err)
+
+		_, err = io.Copy(h, f)
+		return
+	})
+	return
+}
